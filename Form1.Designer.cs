b9fc7daf9b26d8a461484747f16079987030e52b
Prajwalabraham <prajwalabraham.21@gmail.com>
2022-09-02 01:15:20 +0530

    Update

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace FileExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnFront = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtPath1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.shellTreeView1 = new GongSolutions.Shell.ShellTreeView();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 12);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(69, 42);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "<<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnFront
+            // 
+            this.btnFront.Location = new System.Drawing.Point(87, 12);
+            this.btnFront.Name = "btnFront";
+            this.btnFront.Size = new System.Drawing.Size(69, 42);
+            this.btnFront.TabIndex = 1;
+            this.btnFront.Text = ">>";
+            this.btnFront.UseVisualStyleBackColor = true;
+            this.btnFront.Click += new System.EventHandler(this.btnFront_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(1002, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(69, 42);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtPath1
+            // 
+            this.txtPath1.AutoSize = true;
+            this.txtPath1.Location = new System.Drawing.Point(163, 23);
+            this.txtPath1.Name = "txtPath1";
+            this.txtPath1.Size = new System.Drawing.Size(37, 16);
+            this.txtPath1.TabIndex = 3;
+            this.txtPath1.Text = "Path:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(214, 22);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(782, 22);
+            this.txtPath.TabIndex = 4;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(214, 61);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(857, 488);
+            this.webBrowser.TabIndex = 5;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            // 
+            // shellTreeView1
+            // 
+            this.shellTreeView1.Location = new System.Drawing.Point(12, 61);
+            this.shellTreeView1.Name = "shellTreeView1";
+            this.shellTreeView1.Size = new System.Drawing.Size(195, 488);
+            this.shellTreeView1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 561);
+            this.Controls.Add(this.shellTreeView1);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.txtPath1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnFront);
+            this.Controls.Add(this.btnBack);
+            this.Name = "Form1";
+            this.Text = "File Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnFront;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label txtPath1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private GongSolutions.Shell.ShellTreeView shellTreeView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+
+            if (webBrowser.CanGoBack)
+                webBrowser.GoBack();
+        }
+
+        private void btnFront_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoForward)
+                webBrowser.GoForward();
+        }
+
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            //Open browser dialog allows you to select the path
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select your path." })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    webBrowser.Url = new Uri(fbd.SelectedPath);
+                    txtPath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
